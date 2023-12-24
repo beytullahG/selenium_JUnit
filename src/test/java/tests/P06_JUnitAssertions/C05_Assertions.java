f71d6eff38706fe994af92b00f5760e7e5cee0e9
Beytullah Gorgulu <bytllh.grgl@gmail.com>
2023-12-24 13:50:14 +0300

    20231224

---
+++ src/test/java/tests/P06_JUnitAssertions/C05_Assertions.java
@@ -0,0 +1,35 @@
+package tests.P06_JUnitAssertions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C05_Assertions {
+
+    //) Go to https://www.bestbuy.com/
+    // Create different test methods to perform the following tests
+    //		○ urlTest => Test if the page URL is equal to https://www.bestbuy.com/
+    //		○ titleTest => Test if the page title does not contain "Rest"
+    //		○ logoTest => Test if the BestBuy logo is displayed
+    //		○ francaisLinkTest => Test if the French link is displayed
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.bestbuy.com/");
+    }
+
+}
