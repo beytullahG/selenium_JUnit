d314d5260de369f4d606f0def4b3d7e890fd53f8
Beytullah Gorgulu <bytllh.grgl@gmail.com>
2023-12-25 15:15:32 +0300

    20231225

---
+++ src/test/java/tests/P06_JUnitAssertions/C06_CheckBox.java
@@ -0,0 +1,35 @@
+package tests.P06_JUnitAssertions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utilities.ReusableMethods;
+
+import java.time.Duration;
+
+public class C06_CheckBox {
+
+    WebDriver driver;
+
+    @Before
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @After
+    public void teardown(){
+        driver.quit();
+    }
+
+
+}
