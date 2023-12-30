13fafb1d6a61ec7ac8bbf7b7d705b315684c43f1
Beytullah Gorgulu <bytllh.grgl@gmail.com>
2023-12-30 18:11:56 +0300

    20231230

---
+++ src/test/java/tests/P07_testBaseClass_Dropdown/C02_TestBaseFirstTest.java
@@ -0,0 +1,18 @@
+package tests.P07_testBaseClass_Dropdown;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import utilities.ReusableMethods;
+import utilities.TestBase;
+
+public class C02_TestBaseFirstTest extends TestBase {
+
+    @Test
+    public void searchTest(){
+
+
+    }
+}
