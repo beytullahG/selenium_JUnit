c681f086d02bb38f4039737a0e7a8a27f8fd0d99
Beytullah Gorgulu <bytllh.grgl@gmail.com>
2024-01-06 02:09:06 +0300

    20240106

---
+++ src/test/java/tests/P08_iFrame_multipleWindows/C01_BasicAuthentication.java
@@ -0,0 +1,14 @@
+package tests.P08_iFrame_multipleWindows;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.ReusableMethods;
+import utilities.TestBase;
+
+public class C01_BasicAuthentication extends TestBase {
+
+
+}
+
