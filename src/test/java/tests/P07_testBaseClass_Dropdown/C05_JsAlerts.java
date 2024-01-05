08ea95dae20819bfc2c187a04c694022d2107aab
Beytullah Gorgulu <bytllh.grgl@gmail.com>
2024-01-05 15:51:35 +0300

    20240105

--- src/test/java/tests/P07_testBaseClass_Dropdown/C05_JsAlerts.java
+++ src/test/java/tests/P07_testBaseClass_Dropdown/C05_JsAlerts.java
@@ -11,5 +11,21 @@ public class C05_JsAlerts extends TestBase {
 
     // Create 3 test methods and complete the following tasks
 
+    @Test
+    public void jsBasicAlert(){
+        // Test 1
+        //   - Go to https://testotomasyonu.com/javascriptAlert
+        driver.get("https://testotomasyonu.com/javascriptAlert");
+        //   - Click on the first alert
+        driver.findElement(By.xpath("//*[@onclick = 'jsAlert()']")).click();
+        //   - Test that the text in the alert is "I am a JS Alert"
+        String expectedAlertText = "I am a JS Alert";
+        String actualAlertText = driver.switchTo().alert().getText();
+        Assert.assertEquals(expectedAlertText, actualAlertText);
+        //   - Click on the OK button to close the alert
+        ReusableMethods.wait(3);
+        driver.switchTo().alert().accept();
+    }
+
 
 }
