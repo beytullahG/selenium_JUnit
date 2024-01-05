06c3e30a05cbc01724f5e567d63ad541e60cfda8
Beytullah Gorgulu <bytllh.grgl@gmail.com>
2024-01-06 02:09:55 +0300

    20240106

--- src/test/java/tests/P08_iFrame_multipleWindows/C01_BasicAuthentication.java
+++ src/test/java/tests/P08_iFrame_multipleWindows/C01_BasicAuthentication.java
@@ -9,6 +9,20 @@ import utilities.TestBase;
 
 public class C01_BasicAuthentication extends TestBase {
 
+    @Test
+    public void basicAuthenticationTest(){
 
+        //1- Create a class: BasicAuthentication
+        //2- Go to https://testotomasyonu.com/basicauth page
+        // driver.get("https://testotomasyonu.com/basicauth");
+        //3- Authenticate using the following method and test data
+        //
+        //HTML command: https://username:password@URL
+        //  Username: membername
+        //  Password: sunflower
+        //
+
+
+    }
 }
 
