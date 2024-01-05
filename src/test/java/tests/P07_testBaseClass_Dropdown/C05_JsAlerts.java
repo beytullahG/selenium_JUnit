997023ec904ff146a53a1156a9e28ea1bffa0744
Beytullah Gorgulu <bytllh.grgl@gmail.com>
2024-01-05 15:51:04 +0300

    20240105

---
+++ src/test/java/tests/P07_testBaseClass_Dropdown/C05_JsAlerts.java
@@ -0,0 +1,15 @@
+package tests.P07_testBaseClass_Dropdown;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.ReusableMethods;
+import utilities.TestBase;
+
+public class C05_JsAlerts extends TestBase {
+
+    // Create 3 test methods and complete the following tasks
+
+
+}
