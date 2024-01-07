51e30985e5609bbd66e51a6f68ebc81ce4497d36
Beytullah Gorgulu <bytllh.grgl@gmail.com>
2024-01-08 02:05:33 +0300

    20240108

---
+++ src/test/java/tests/P08_iFrame_multipleWindows/C03_IFrame.java
@@ -0,0 +1,19 @@
+package tests.P08_iFrame_multipleWindows;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.ReusableMethods;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class C03_IFrame extends TestBase {
+
+    @Test
+    public void test01(){
+        // Go to http://demo.guru99.com/test/guru99home/
+        driver.get("http://demo.guru99.com/test/guru99home/");
+
+    }
+}
