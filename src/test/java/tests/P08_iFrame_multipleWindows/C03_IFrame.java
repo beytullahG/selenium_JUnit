fb49ff2d3b67b7b2d89d509a84ed1076d0a54dc5
Beytullah Gorgulu <bytllh.grgl@gmail.com>
2024-01-08 02:10:32 +0300

    20240108

--- src/test/java/tests/P08_iFrame_multipleWindows/C03_IFrame.java
+++ src/test/java/tests/P08_iFrame_multipleWindows/C03_IFrame.java
@@ -14,6 +14,17 @@ public class C03_IFrame extends TestBase {
     public void test01(){
         // Go to http://demo.guru99.com/test/guru99home/
         driver.get("http://demo.guru99.com/test/guru99home/");
+        // Accept Cookies
+        // Since the cookies are in an iframe,
+        // switch to that iframe first
+        WebElement cookiesIFrame = driver.findElement(By.xpath("(//iframe)[4]"));
+        driver.switchTo().frame(cookiesIFrame);
+        driver.findElement(By.xpath("//*[text()='Accept All']")).click();
+
+        // Find the number of iframes on the page
+        List<WebElement> iFrameList = driver.findElements(By.tagName("iframe"));
+        System.out.println("Number of iframes on the page: " + iFrameList.size());
+
 
     }
 }
