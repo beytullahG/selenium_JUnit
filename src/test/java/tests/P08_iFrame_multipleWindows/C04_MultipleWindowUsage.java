9349a29c485a519abcf20a56910f444c293b67db
Beytullah Gorgulu <bytllh.grgl@gmail.com>
2024-01-09 03:29:21 +0300

    20240109

---
+++ src/test/java/tests/P08_iFrame_multipleWindows/C04_MultipleWindowUsage.java
@@ -0,0 +1,21 @@
+package tests.P08_iFrame_multipleWindows;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.ReusableMethods;
+import utilities.TestBase;
+
+public class C04_MultipleWindowUsage extends TestBase {
+
+    @Test
+    public void test01(){
+
+        // In all the operations we perform with the driver,
+        // we work in the same window by default.
+        // Therefore, the driver searches for every action we perform
+        // in the same window. If you want, you can create a new tab or window.
+
+
+    }
+}
+
